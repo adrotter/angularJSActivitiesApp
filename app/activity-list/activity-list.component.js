cbfcac2abc5b7f9ff0269773d4de9c849cd483f7
adrotter <adrotter@ucdavis.edu>
2017-05-11 21:43:14 -0700

    completed steps 1,2,3,4,5. needs materials

--- app/activity-list/activity-list.component.js
+++ app/activity-list/activity-list.component.js
@@ -163,18 +163,17 @@ angular.
   }).
   directive('clock', function ($interval) {
   return {
-    scope: true, // isolate
-    transclude: true, // bring in any text or elements
-    template: "<span class='clock'><span class='clock-text' ng-transclude></span><span class='clock-time'>{{date.now() | date: timeFormat}}</span></span>",
-    /**
-     * $s scope
-     * $e element
-     * $a attributes
-     */
-    link: function ($s, $e, $a) {
-        $s.timeFormat = ($a.format === '12') ? 'hh:mm:ss a' : 'HH:mm:ss';
-        $s.date = Date;
-        $interval(null, 1000);
-    }
-  };
+      restrict:'E',
+      scope: {
+         date: '@'
+      },
+      template:'<div class="date">{{difference}}</div>',
+      link: function(scope, element, attrs) {
+        var updateTime = function(){
+          scope.difference =  moment(scope.date).fromNow();
+        }
+        
+        $interval(updateTime, 1000);
+      }
+   };
 });
